3cea9d49d7249d0e45081fdf808e07a268493bf0
Ivica Batinic <ivica.batinic@infinum.hr>
2020-03-19 01:27:46 +0100

    WIP on settig up gh pages

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,7 +1,11 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
-})
+});
+
+const development = process.env.NODE_ENV !== 'production';
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-})
+  // assetPrefix: !development ? '/theme-ui-expansion-pack/' : '',
+  configOrigin: !development ? 'https://isbatak.github.io/theme-ui-expansion-pack/' : 'default',
+});
