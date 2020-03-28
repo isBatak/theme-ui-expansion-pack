465ca69db44fbc5dd03c160c594913f6508ac779
Ivica Batinic <ivica.batinic@infinum.hr>
2020-03-28 22:51:26 +0100

    Fix favicons

--- packages/docs/src/components/Meta.tsx
+++ packages/docs/src/components/Meta.tsx
@@ -7,14 +7,14 @@ export const Meta = () => {
 
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/theme-ui-expansion-pack/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/theme-ui-expansion-pack/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/theme-ui-expansion-pack/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/theme-ui-expansion-pack/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/theme-ui-expansion-pack/favicon/safari-pinned-tab.svg" color="#000000" />
+      <link rel="shortcut icon" href="/theme-ui-expansion-pack/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content={theme.colors.background} />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="msapplication-config" content="/theme-ui-expansion-pack/favicon/browserconfig.xml" />
       <meta name="theme-color" content={theme.colors.background} />
     </Head>
   );
