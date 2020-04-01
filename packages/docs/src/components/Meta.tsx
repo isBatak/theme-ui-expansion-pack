27001cd62622d60ae4953ff251de7b72aba80978
Ivica Batinic <ivica.batinic@infinum.hr>
2020-04-01 20:56:51 +0200

    FIx manifest error

--- packages/docs/src/components/Meta.tsx
+++ packages/docs/src/components/Meta.tsx
@@ -5,16 +5,18 @@ import { useThemeUI } from 'theme-ui';
 export const Meta = () => {
   const { theme } = useThemeUI();
 
+  const urlPrefix = process.env.NODE_ENV === 'production' ? '/theme-ui-expansion-pack/favicon' : '';
+
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/theme-ui-expansion-pack/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/theme-ui-expansion-pack/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/theme-ui-expansion-pack/favicon/favicon-16x16.png" />
-      <link rel="manifest" href="/theme-ui-expansion-pack/favicon/site.webmanifest" />
-      <link rel="mask-icon" href="/theme-ui-expansion-pack/favicon/safari-pinned-tab.svg" color="#000000" />
-      <link rel="shortcut icon" href="/theme-ui-expansion-pack/favicon/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href={`${urlPrefix}/favicon/apple-touch-icon.png`} />
+      <link rel="icon" type="image/png" sizes="32x32" href={`${urlPrefix}/favicon/favicon-32x32.png`} />
+      <link rel="icon" type="image/png" sizes="16x16" href={`${urlPrefix}/favicon/favicon-16x16.png`} />
+      <link rel="manifest" href={`${urlPrefix}/manifest.json`} />
+      <link rel="mask-icon" href={`${urlPrefix}/favicon/safari-pinned-tab.svg" color="#000000`} />
+      <link rel="shortcut icon" href={`${urlPrefix}/favicon/favicon.ico`} />
       <meta name="msapplication-TileColor" content={theme.colors.background} />
-      <meta name="msapplication-config" content="/theme-ui-expansion-pack/favicon/browserconfig.xml" />
+      <meta name="msapplication-config" content={`${urlPrefix}/favicon/browserconfig.xml`} />
       <meta name="theme-color" content={theme.colors.background} />
     </Head>
   );
