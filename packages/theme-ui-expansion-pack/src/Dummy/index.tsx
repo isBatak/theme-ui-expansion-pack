cdfd5180ddb6faadf0c4b0b82f3851ec37ba31c0
kristian240 <kristian.djakovic@infinum.hr>
2020-08-30 18:45:24 +0200

    Add height and width props support

--- packages/theme-ui-expansion-pack/src/Dummy/index.tsx
+++ packages/theme-ui-expansion-pack/src/Dummy/index.tsx
@@ -1,21 +1,32 @@
 import React, { forwardRef } from 'react';
 import { Box, BoxProps } from 'theme-ui';
 
-export const Dummy = forwardRef<HTMLDivElement, BoxProps>(({ sx, ...rest }, ref) => (
-  <Box
-    as="span"
-    ref={ref}
-    sx={{
-      bg: 'neutral',
-      height: '1rem',
-      borderRadius: '2px',
-      display: 'inline-block',
-      lineHeight: 'inherit',
-      maxWidth: '100%',
-      ...sx,
-    }}
-    {...rest}
-  >
-    &nbsp;
-  </Box>
-));
+interface IDummyProps extends BoxProps {
+  width?: number | string;
+  height?: number | string;
+}
+
+export const Dummy = forwardRef<HTMLDivElement, IDummyProps>(function(
+  { sx, width = '100%', height = '1rem', ...rest },
+  ref,
+) {
+  return (
+    <Box
+      as="span"
+      ref={ref}
+      sx={{
+        bg: 'neutral',
+        height,
+        width,
+        borderRadius: '2px',
+        display: 'inline-block',
+        lineHeight: 'inherit',
+        maxWidth: '100%',
+        ...sx,
+      }}
+      {...rest}
+    >
+      &nbsp;
+    </Box>
+  );
+});
